9935e408a788c4dd617faea65a8353ac44eeaf6d
Ntholi Nkhatho <ntholi@live.com>
2023-11-29 11:15:55 +0200

    Add home page link to navbar

--- src/app/(admin)/appshell/Navbar.tsx
+++ src/app/(admin)/appshell/Navbar.tsx
@@ -3,6 +3,7 @@ import {
   IconBrandWechat,
   IconCalendarEvent,
   IconChevronRight,
+  IconHome,
 } from '@tabler/icons-react';
 import Link from 'next/link';
 import React from 'react';
@@ -11,15 +12,15 @@ export default function Navbar() {
   return (
     <nav>
       <NavLink
+        label='Home Page'
+        leftSection={<IconHome size='1rem' stroke={1.5} />}
+        rightSection={<IconChevronRight size='0.8rem' stroke={1.5} />}
+      />
+      <NavLink
         label='Events'
         component={Link}
         href={'/admin/events'}
         leftSection={<IconCalendarEvent size='1rem' stroke={1.5} />}
-        rightSection={<IconChevronRight size='0.8rem' stroke={1.5} />}
-      />
-      <NavLink
-        label='Forum'
-        leftSection={<IconBrandWechat size='1rem' stroke={1.5} />}
         rightSection={<IconChevronRight size='0.8rem' stroke={1.5} />}
       />
     </nav>
