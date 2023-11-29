8a624f2c2ccc2ccc66ca41c2be213637b8aa081a
Ntholi Nkhatho <ntholi@live.com>
2023-11-29 21:38:25 +0200

    Refactor import statements and update class names

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,10 @@
-import { db } from '@/lib/firebase';
-import { MdHotel, MdFastfood, MdCelebration } from 'react-icons/md';
-import { doc, getDoc } from 'firebase/firestore';
-import Link from 'next/link';
+import { db } from "@/lib/firebase";
+import { MdHotel, MdFastfood, MdCelebration } from "react-icons/md";
+import { doc, getDoc } from "firebase/firestore";
+import Link from "next/link";
 
 async function getPage() {
-  const data = (await getDoc(doc(db, 'pages', 'home-page'))).data();
+  const data = (await getDoc(doc(db, "pages", "home-page"))).data();
   return data as unknown as HomePage;
 }
 
@@ -16,32 +16,32 @@ export default async function Home() {
         style={{
           backgroundImage: `url(${page.banner})`,
         }}
-        className={`hero h-screen flex flex-col justify-center items-center bg-blend-overlay  bg-black/40 bg-center bg-cover text-white`}
+        className={`hero flex h-screen flex-col items-center justify-center bg-black/40  bg-cover bg-center text-white bg-blend-overlay`}
       >
         <header>
-          <h1 className='border-white border-3 px-10 py-5 text-6xl uppercase'>
+          <h1 className="border-3 border-white px-10 py-5 text-7xl uppercase">
             Thabeng
           </h1>
-          <p className='mt-5 uppercase tracking-wide text-center'>
+          <p className="mt-5 text-center uppercase tracking-wide">
             {page.tagline}
           </p>
         </header>
-        <nav className='mt-28 flex gap-10'>
-          <Link href='#' className='flex flex-col gap-2 items-center'>
-            <div className='border p-7 rounded-md bg-black/60 text-white'>
-              <MdFastfood size='2rem' />
+        <nav className="mt-28 flex gap-16">
+          <Link href="#" className="flex flex-col items-center gap-2">
+            <div className="rounded-md border bg-black/60 p-7 text-amber-100">
+              <MdFastfood size="2rem" />
             </div>
             Restaurant
           </Link>
-          <Link href='#' className='flex flex-col gap-2 items-center'>
-            <div className='border p-7  rounded-md bg-black/60 text-white'>
-              <MdCelebration size='2rem' />
+          <Link href="#" className="flex flex-col items-center gap-2">
+            <div className="rounded-md border  bg-black/60 p-7 text-amber-100">
+              <MdCelebration size="2rem" />
             </div>
             Events
           </Link>
-          <Link href='#' className='flex flex-col gap-2 items-center'>
-            <div className='border p-7  rounded-md bg-black/60 text-white'>
-              <MdHotel size='2rem' />
+          <Link href="#" className="flex flex-col items-center gap-2">
+            <div className="rounded-md border  bg-black/60 p-7 text-amber-100">
+              <MdHotel size="2rem" />
             </div>
             Bookings
           </Link>
