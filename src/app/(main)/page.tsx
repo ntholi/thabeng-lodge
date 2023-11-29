a1d3f21c1c7a9e9bc1f86851cf02eaf0f8a8abaf
Ntholi Nkhatho <ntholi@live.com>
2023-11-29 12:47:06 +0200

    display server background image on home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,17 @@ export default async function Home() {
   const page = (await getDoc(doc(db, 'pages', 'home-page'))).data() as HomePage;
   return (
     <>
-      <header className='mt-20'>here: {page.tagline}</header>
+      <header
+        style={{
+          backgroundImage: `url(${page.banner})`,
+        }}
+        className={`h-screen flex flex-col justify-center items-center bg-blend-overlay  bg-black/60 bg-center bg-cover text-white`}
+      >
+        <h1 className='border border-white px-10 py-5 text-3xl uppercase'>
+          Thabeng Lodge
+        </h1>
+        <p className='mt-5 text-xl'>{page.tagline}</p>
+      </header>
       <main>
         <Button>Click Me</Button>
       </main>
