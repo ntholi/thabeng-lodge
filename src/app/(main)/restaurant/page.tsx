6c0dfd12622199469a433ccd5987701d0308887d
Ntholi Nkhatho <ntholi@live.com>
2023-11-29 15:37:09 +0200

    install prettier-plugin-tailwindcss

--- src/app/(main)/restaurant/page.tsx
+++ src/app/(main)/restaurant/page.tsx
@@ -1,24 +1,26 @@
-import { db } from '@/lib/firebase';
-import { doc, getDoc } from 'firebase/firestore';
-import React from 'react';
+import { db } from "@/lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import React from "react";
 
 async function getPage() {
-  const data = (await getDoc(doc(db, 'pages', 'restaurant-page'))).data();
+  const data = (await getDoc(doc(db, "pages", "restaurant-page"))).data();
   return data as unknown as RestaurantPage;
 }
 
 export default async function RestaurantPage() {
   const page = await getPage();
   return (
-    <main
-      style={{
-        backgroundImage: `url(${page.banner})`,
-      }}
-      className={`h-[40vh] flex flex-col justify-center items-center bg-blend-overlay  bg-black/60 bg-center bg-cover text-white`}
-    >
-      <header>
-        <h2 className='text-3xl'>{page.description}</h2>
+    <>
+      <header
+        style={{
+          backgroundImage: `url(${page.banner})`,
+        }}
+        className={`flex h-[40vh] flex-col items-center justify-center bg-black/60  bg-cover bg-center text-white bg-blend-overlay`}
+      >
+        <h2 className="text-center text-5xl font-bold">Restaurant</h2>
+        <h2 className="mt-3 text-center text-lg">{page.description}</h2>
       </header>
-    </main>
+      <main className="container mx-auto mt-10 px-4"></main>
+    </>
   );
 }
