e87a25fd377b2f57a645de9cfe7f123743f13088
Ntholi Nkhatho <ntholi@live.com>
2023-11-30 11:16:10 +0200

    Style events section

--- src/app/(main)/home/EventList.tsx
+++ src/app/(main)/home/EventList.tsx
@@ -1,6 +1,7 @@
 import { DocumentData, QuerySnapshot } from "firebase/firestore";
 import React from "react";
 import { Event } from "../events/modals";
+import { MdOutlineEvent } from "react-icons/md";
 
 type Props = {
   promiseDocs: Promise<QuerySnapshot<DocumentData, DocumentData>>;
@@ -18,10 +19,34 @@ export default async function EventList({ promiseDocs }: Props) {
   return (
     <div>
       {data.map((item) => (
-        <article key={item.id}>
-          <h3>{item.title}</h3>
+        <article
+          key={item.id}
+          className="mb-5 flex items-center gap-5 border-b border-zinc-500 pb-3"
+        >
+          <div className="rounded-full bg-white p-3 text-black">
+            <MdOutlineEvent className="text-xl" />
+          </div>
+          <div>
+            <h3 className="font-semibold">{item.title}</h3>
+            <p className="text-sm">{toDate(item.date)}</p>
+            <p className="text-sm">{shorten(item.description)}</p>
+          </div>
         </article>
       ))}
     </div>
   );
+}
+
+function shorten(str: string) {
+  return str.length > 35 ? str.substring(0, 35) + "..." : str;
+}
+
+function toDate(date: Date) {
+  console.log("date", date);
+  return new Date(date).toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 }
--- src/app/(main)/home/EventsSection.tsx
+++ src/app/(main)/home/EventsSection.tsx
@@ -3,12 +3,16 @@ import React, { Suspense } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 import EventList from "./EventList";
+import { MdAccessTime } from "react-icons/md";
 
 export default function EventsSection() {
   const menuItems = getDocs(collection(db, "events"));
   return (
-    <section>
-      <h2 className="text-3xl font-bold">Events</h2>
+    <section className="rounded-md bg-zinc-900 p-5 text-white">
+      <header className="flex justify-between">
+        <h2 className="text-3xl font-bold">Events</h2>
+        <MdAccessTime className="text-3xl" />
+      </header>
       <Divider className="mt-1" />
       <Suspense fallback={<div>Loading...</div>}>
         <div className="pt-10">
