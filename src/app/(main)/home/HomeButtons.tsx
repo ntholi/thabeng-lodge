2f451e9fc72c3981f1b6334bace5866290217af8
Ntholi Nkhatho <ntholi@live.com>
2024-02-02 15:16:01 +0200

    Update link component in HomeButtons.tsx

--- src/app/(main)/home/HomeButtons.tsx
+++ src/app/(main)/home/HomeButtons.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import NextLink from "next/link";
 import { MdHotel, MdFastfood, MdCelebration } from "react-icons/md";
 
 export default function HomeButtons() {
@@ -15,7 +15,7 @@ export default function HomeButtons() {
           transition: { type: "spring", stiffness: 50 },
         }}
       >
-        <Link href="/restaurant" className="flex flex-col items-center gap-2">
+        <Link href="/restaurant">
           <div className="rounded-md border bg-black/60 p-7 text-white">
             <MdFastfood className="text-xl sm:text-3xl" />
           </div>
@@ -30,7 +30,7 @@ export default function HomeButtons() {
           transition: { delay: 0.2, type: "spring", stiffness: 50 },
         }}
       >
-        <Link href="/events" className="flex flex-col items-center gap-2">
+        <Link href="/events">
           <div className="rounded-md border  bg-black/60 p-7 text-white">
             <MdCelebration className="text-xl sm:text-3xl" />
           </div>
@@ -45,13 +45,24 @@ export default function HomeButtons() {
           transition: { delay: 0.3, type: "spring", stiffness: 50 },
         }}
       >
-        <Link href="#" className="flex flex-col items-center gap-2">
+        <Link href="/rooms">
           <div className="rounded-md border  bg-black/60 p-7 text-white">
             <MdHotel className="text-xl sm:text-3xl" />
           </div>
-          Bookings
+          Rooms
         </Link>
       </motion.div>
     </>
+  );
+}
+
+function Link({ href, children }: { href: string; children: React.ReactNode }) {
+  return (
+    <NextLink
+      href={href}
+      className="flex w-24 flex-col items-center gap-2 rounded-md p-2 text-center transition-all ease-in-out hover:scale-110 sm:w-32"
+    >
+      {children}
+    </NextLink>
   );
 }
