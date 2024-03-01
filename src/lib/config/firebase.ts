a8aed2f48d383f3a8ebc7d9508afce54970052c6
Ntholi Nkhatho <ntholi@live.com>
2024-03-01 11:39:20 +0200

    Update Firebase configuration

--- src/lib/config/firebase.ts
+++ src/lib/config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCla13GCb3mso9JfTna4e10qDoNaAlj1SM",
-  authDomain: "thabeng-hotel.firebaseapp.com",
-  projectId: "thabeng-hotel",
-  storageBucket: "thabeng-hotel.appspot.com",
-  messagingSenderId: "866728967458",
-  appId: "1:866728967458:web:0a7f704fd4301b126fb97b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
